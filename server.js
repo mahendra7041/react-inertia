995fb28a1bfa200a17703ad63e46f80fff566c60
mahendra7041 <chavdamahendra247@gmail.com>
2025-08-25 23:31:13 +0530

    refactor: bootstrap function removed

--- server.js
+++ server.js
@@ -2,27 +2,22 @@ import express from "express";
 import router from "./app/router.js";
 import { inertiaMiddleware } from "./app/utils/inertia.js";
 
-async function bootstrap() {
-  const app = express();
-  const PORT = process.env.PORT || 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-  app.use(express.static("public"));
+app.use(express.static("public"));
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(
-      express.static("build/client", {
-        index: false,
-      })
-    );
-  }
-
-  app.use(inertiaMiddleware);
-
-  app.use(router);
-
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-  });
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static("build/client", {
+      index: false,
+    })
+  );
 }
 
-bootstrap();
+app.use(inertiaMiddleware);
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});

ba1edc1434d8367e0f4f0a3f0300666a17eb97fc
mahendra7041 <chavdamahendra247@gmail.com>
2025-08-26 15:59:35 +0530

    refactor: improve folder structure

--- app/utils/inertia.js
+++
@@ -1,4 +0,0 @@
-import inertia from "express-inertia";
-import { inertiaConfig } from "../../configs/inertia.config.js";
-
-export const inertiaMiddleware = await inertia(inertiaConfig);
--- server.js
+++ server.js
@@ -1,23 +1,52 @@
 import express from "express";
-import router from "./app/router.js";
-import { inertiaMiddleware } from "./app/utils/inertia.js";
+import { inertiaMiddleware } from "express-inertia";
+import { createServer } from "vite";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+async function bootstrap() {
+  const app = express();
+  const PORT = process.env.PORT || 5000;
 
-app.use(express.static("public"));
+  app.use(express.static("public"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(
-    express.static("build/client", {
-      index: false,
-    })
-  );
+  if (process.env.NODE_ENV === "production") {
+    app.use(
+      express.static("build/client", {
+        index: false,
+      })
+    );
+  }
+
+  const vite = await createServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+  });
+  app.use(vite.middlewares);
+
+  const config = {
+    encryptHistory: true,
+    client: {
+      entrypoint: "index.html",
+      bundle: "build/client/index.html",
+    },
+    ssr: {
+      entrypoint: "src/ssr.jsx",
+      bundle: "src/ssr/ssr.js",
+    },
+  };
+
+  app.use(inertiaMiddleware(config, vite));
+
+  app.get("/", (req, res) => {
+    res.inertia.render("home");
+  });
+
+  app.get("/about", (req, res) => {
+    res.inertia.render("about");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
 }
 
-app.use(inertiaMiddleware);
-app.use(router);
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+bootstrap();
